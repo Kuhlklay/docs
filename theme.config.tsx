69243cc9eb188e23c7b280001f9e1b3e7886d121
Kuhlklay <124497095+Kuhlklay@users.noreply.github.com>
2025-02-07 14:33:33 +0100

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Kvexium Docs</span>,
+  logo: <span><img src="src/img/logo.png" style="border: 1px solid white; border-radius: 4px;"/> Kvexium Docs</span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
